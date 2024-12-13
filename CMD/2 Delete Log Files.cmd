f8d549db61236926187a486d2bd44fa5201d04b0
abdeljalil52 <96989559+abdeljalil52@users.noreply.github.com>
2024-12-13 18:02:30 +0100

    2 Delete Log Files.cmd

--- CMD/2 Delete Log Files.cmd
+++ CMD/2 Delete Log Files.cmd
@@ -1,5 +1,16 @@
 @echo off
-cd/
-@echo
+echo Deleting .log files...
+
+:: Navigate to the root directory
+cd /
+
+:: Delete all .log files
 del *.log /a /s /q /f
+
+:: Easy to spot message
+echo ============================================================
+echo All .log files deleted successfully.
+echo Developed by Smuley
+echo ============================================================
+
 pause
