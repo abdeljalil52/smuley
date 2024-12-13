12e0fa7b16e22a4b6f85a5cb73a4db427f3fb72a
abdeljalil52 <96989559+abdeljalil52@users.noreply.github.com>
2024-12-13 18:01:38 +0100

    Delete Temporary Files.cmd

--- CMD/1 Delete Temporary Files.cmd
+++ CMD/1 Delete Temporary Files.cmd
@@ -1,11 +1,22 @@
 @echo off
-echo Batch File By Adamx (YouTube.Com/AdamxYT)
+echo Cleaning temporary directories...
+
+:: Clean user-specific temp folder
 RD /S /Q %temp%
 MKDIR %temp%
 takeown /f "%temp%" /r /d y
+
+:: Clean system-wide temp folder
 takeown /f "C:\Windows\Temp" /r /d y
 RD /S /Q C:\Windows\Temp
 MKDIR C:\Windows\Temp
 takeown /f "C:\Windows\Temp" /r /d y
-takeown /f %temp% /r /d y
+
+echo Temp directories cleaned successfully.
+
+:: Easy to spot message
+echo ============================================================
+echo This script was Developed by Smuley!
+echo ============================================================
+
 pause
