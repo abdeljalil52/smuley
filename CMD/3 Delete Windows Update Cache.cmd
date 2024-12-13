67f05858ad3706fc12f53ae12e04fd8385fde0e6
abdeljalil52 <96989559+abdeljalil52@users.noreply.github.com>
2024-12-13 18:17:04 +0100

    3 Delete Windows Update Cache.cmd

--- CMD/3 Delete Windows Update Cache.cmd
+++ CMD/3 Delete Windows Update Cache.cmd
@@ -1,7 +1,15 @@
 @echo off
-echo Batch File By Adamx
+color 3
+echo.
+
 net stop wuauserv
 net stop UsoSvc
 rd /s /q C:\Windows\SoftwareDistribution
 md C:\Windows\SoftwareDistribution
+
+echo.
+echo ============================================================
+echo Windows Updates have been successfully disabled.
+echo Developed by Smuley
+echo ============================================================
 pause
