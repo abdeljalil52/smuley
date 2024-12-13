08d16663c8a5bafd94fc4a86ae66f4e3ad13df78
abdeljalil52 <96989559+abdeljalil52@users.noreply.github.com>
2024-12-13 20:38:47 +0100

    Disable HPET.cmd

--- CMD/Disable HPET.cmd
+++ CMD/Disable HPET.cmd
@@ -1,22 +1,23 @@
 @echo off
+color 3
 title Disable HPET
-echo ==================================================
-echo              Disable HPET and Optimize Timers
-echo             Developed by SmuLeY
-echo ==================================================
 
-echo Disabling HPET settings...
+echo.
+echo [-] Disabling HPET settings...
 bcdedit /deletevalue useplatformclock
 
 echo.
-echo Disabling dynamic tick (laptop power savings)...
+echo [-] Disabling dynamic tick (laptop power savings)...
 bcdedit /set disabledynamictick yes
 
 echo.
-echo Enabling synthetic timers...
+echo [-] Enabling synthetic timers...
 bcdedit /set useplatformtick yes
 
 echo.
-echo HPET has been successfully disabled and system timers optimized!
+echo [+] HPET has been successfully disabled and system timers optimized!
 echo ==================================================
-pause
+echo              Disable HPET and Optimize Timers
+echo             Developed by Smuley
+echo ==================================================
+pause >nul
