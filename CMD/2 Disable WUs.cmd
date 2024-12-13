128770ad3a1c2ec95082f2b1025231379012d5be
abdeljalil52 <96989559+abdeljalil52@users.noreply.github.com>
2024-12-13 18:03:11 +0100

    2 Disable WUs.cmd

--- CMD/2 Disable WUs.cmd
+++ CMD/2 Disable WUs.cmd
@@ -1,15 +1,34 @@
 @echo off
+echo Disabling Windows Update services and related settings...
+
+:: Kill the SystemSettings process
 taskkill /F /FI "IMAGENAME eq SystemSettings.exe"
+
+:: Stop Windows Update services
 net stop wuauserv
 net stop UsoSvc
+
+:: Modify registry to disable Windows Update
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "DoNotConnectToWindowsUpdateInternetLocations" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "SetDisableUXWUAccess" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /t REG_DWORD /d "1" /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ExcludeWUDriversInQualityUpdate" /t REG_DWORD /d "1" /f
+
+:: Force Group Policy update
 gpupdate /force
+
+:: Reset SoftwareDistribution folder
 rd /s /q C:\Windows\SoftwareDistribution
 md C:\Windows\SoftwareDistribution
+
+:: Restart Windows Update services
 net start wuauserv
 net start UsoSvc
-echo WUs are successfully disabled.
+
+:: Confirmation message
+echo ============================================================
+echo Windows Updates have been successfully disabled.
+echo Developed by Smuley
+echo ============================================================
+
 pause
