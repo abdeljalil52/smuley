ed51a3544db2d0fdf3a5b805a4a33b52aa4c36ab
abdeljalil52 <96989559+abdeljalil52@users.noreply.github.com>
2024-12-13 20:37:15 +0100

    Delete Log Files.cmd

--- CMD/Delete Log Files.cmd
+++ CMD/Delete Log Files.cmd
@@ -1,12 +1,9 @@
 @echo off
+color 3
 title Delete Temporary Files
-echo ==================================================
-echo              Temporary File Cleaner
-echo             Developed by SmuLeY
-echo ==================================================
 
 echo.
-echo Deleting temporary files to free up space and improve performance...
+echo [-] Deleting temporary files to free up space and improve performance...
 
 :: Delete and recreate user temp folder
 RD /S /Q %temp%
@@ -18,6 +15,9 @@ RD /S /Q C:\Windows\Temp
 MKDIR C:\Windows\Temp
 
 echo.
-echo Temporary files successfully deleted!
+echo [+] Temporary files successfully deleted!
 echo ==================================================
-pause
+echo              Temporary File Cleaner
+echo             Developed by Smuley
+echo ==================================================
+pause >nul
