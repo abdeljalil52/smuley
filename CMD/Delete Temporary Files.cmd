b8eb802110b7109d46e19a4d55a58e9213a78a40
abdeljalil52 <96989559+abdeljalil52@users.noreply.github.com>
2024-12-13 20:37:52 +0100

    Delete Temporary Files.cmd

--- CMD/Delete Temporary Files.cmd
+++ CMD/Delete Temporary Files.cmd
@@ -1,9 +1,9 @@
 @echo off
+color 3
 title Delete Temporary Files
-echo ==================================================
-echo              Temporary File Cleaner
-echo             Developed by SmuLeY
-echo ==================================================
+
+echo.
+echo [-] Deleting temporary files to free up space and improve performance...
 
 :: Delete and recreate user temp folder
 RD /S /Q %temp%
@@ -15,4 +15,10 @@ takeown /f "C:\Windows\Temp" /r /d y
 RD /S /Q C:\Windows\Temp
 MKDIR C:\Windows\Temp
 
-pause
+echo.
+echo [+] Temporary files successfully deleted!
+echo ==================================================
+echo              Temporary File Cleaner
+echo             Developed by Smuley
+echo ==================================================
+pause >nul
